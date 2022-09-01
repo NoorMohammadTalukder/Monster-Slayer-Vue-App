8348c5cf06baf57a9ee357f99e96e4d18d35a277
YourName <noor11809027@gmail.com>
2022-09-01 16:44:44 +0600

    project end

--- app.js
+++ app.js
@@ -9,13 +9,20 @@ const app = Vue.createApp({
       monsterHealth: 100,
       currentRound: 0,
       winner:null,
+      logMessages:[],
     };
   },
   computed: {
     monsterBarStyles() {
+        if(this.monsterHealth<0){
+            return {width:'0%'}
+        }
       return { width: this.monsterHealth + "%" };
     },
     playerBarStyles() {
+        if(this.playerHealth<0){
+            return {width:'0%'}
+        }
       return { width: this.playerHealth + "%" };
     },
     mayUseSpecialAttack() {
@@ -43,23 +50,26 @@ const app = Vue.createApp({
     },
   },
   methods: {
+     
     attackMonster() {
       // const attackValue=Math.floor(Math.random()*(12-5))+5;
       this.currentRound++;
       const attackValue = getRandom(12, 5);
       this.monsterHealth = this.monsterHealth - attackValue;
-
+      this.addLogMessage('player','attack',attackValue);
       this.attackPlayer();
     },
     attackPlayer() {
       // const attackValue=Math.floor(Math.random()*(15-8))+8;
       const attackValue = getRandom(15, 8);
       this.playerHealth = this.playerHealth - attackValue;
+      this.addLogMessage('monster','attack',attackValue);
     },
     specialAttackMonster() {
       this.currentRound++;
       const attackValue = getRandom(10, 25);
       this.monsterHealth -= attackValue;
+     this.addLogMessage("player","special attack",attackValue);
       this.attackPlayer();
     },
     healPlayer(){
@@ -70,8 +80,27 @@ const app = Vue.createApp({
         }else{
            this.playerHealth+=healValue;
         }
+       this.addLogMessage("player","heal",healValue);
        this.attackPlayer();
-    }
+    },
+    startNewGame(){
+      this.playerHealth= 100;
+      this.monsterHealth= 100;
+      this.currentRound= 0;
+      this.winner=null;
+    },
+    surrender(){
+      this.winner="monster";
+    },
+    addLogMessage(who,what,value){
+      this.logMessages.unshift({
+        actionBy:who,
+        actionType: what,
+        actionValue:value,
+   
+      });
+      console.log(this.logMessages);
+    }  
   },
 });
 app.mount("#game");

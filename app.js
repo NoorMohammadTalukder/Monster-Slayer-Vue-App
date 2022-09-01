5a4ed39d25133fd8df85c412b47e5937295b1214
YourName <noor11809027@gmail.com>
2022-09-01 12:11:59 +0600

    attackMonster(), attackPlayer() added

--- app.js
+++ app.js
@@ -1,4 +1,28 @@
-const app=Vue.createApp({
+function getRandom(min,max){
+    return Math.floor(Math.random()*(max-min))+min;
+}
 
+
+const app=Vue.createApp({
+    data(){
+        return{
+            playerHealth:100,
+            monsterHealth:100,
+        }
+    },
+    methods:{
+        attackMonster(){
+            // const attackValue=Math.floor(Math.random()*(12-5))+5;
+            const attackValue=getRandom(12,5);
+            this.monsterHealth=this.monsterHealth-attackValue;
+
+            this.attackPlayer();
+        },
+        attackPlayer(){
+            // const attackValue=Math.floor(Math.random()*(15-8))+8;
+            const attackValue=getRandom(15,8);
+            this.playerHealth=this.playerHealth-attackValue; 
+        }
+    }
 });
 app.mount('#game');
